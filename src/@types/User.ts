09f04d7680f8a06328c6c675624b2d366462dba9
Nuruthantur <olivermayer123@gmail.com>
2024-03-28 11:21:46 +0100

    update old files deleted

--- src/@types/User.ts
+++ src/@types/User.ts
@@ -6,30 +6,14 @@ export default interface User {
   firstName?: string;
   lastName?: string;
   birthDate?: Date;
-  // profilePicture?: { URL: string; public_Id: string };
   public_id?: string;
-  hometown?: string;
-  travelDates?: Date;
-  favouriteLocations?: string;
   likes?: string;
   requests?: string;
   connections?: string;
+  hometown?: string;
+  aboutYourself?: string;
+  travelingDates?: string;
+  travelingDestinations?: string;
+  agreeToTerms?: boolean;
+  userPicture?: string;
 }
-
-export interface TestUser {
-  _id: string;
-  userName: string;
-  email: string;
-  password: string;
-  favDestinations?: string;
-}
-
-export interface TestUser {
-  _id: string;
-  email: string;
-  password: string;
-  userName: string;
-  favDestinations?: string;
-}
-
-import { ObjectId } from "mongoose";
--- src/app/settings/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function SettingsPage() {
-  return (
-    <div>
-      <h1>this is the users settings page</h1>
-    </div>
-  );
-}
